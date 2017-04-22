1bccc531e78b047ed2aafacece77aa69213c4187
MiaoS <miaoshi2013@gmail.com>
2017-04-22 15:53:09 -0700

    finalize data sample for each diagram and functions to create each diagram

--- js/graphicLib/graphicLibGateway.js
+++ js/graphicLib/graphicLibGateway.js
@@ -2,45 +2,48 @@
  * Created by ss on 2017/4/13.
  */
 /*
-------------- data for dot chart based on 以 x 为时间，y轴为震源深度，圆点半径表示震级，颜色表示alert的颜色； ---------------------
+-------------depthdotchart 以 x 为时间，y轴为震源深度，圆点半径表示震级，颜色表示alert的颜色； ---------------------
 
 [ { 
 mag:number, 
 place：string,
 time:number, 
 alert:string, 
-depth:number,
-url:string   ---- 如果需要点击跳转的话
+depth:number ----geometry.coordinates[3]
+
 },{}....... ]
 
 
-------------- 先放放 data for geo dot  chart base on 以 X 为纬度，Y 为经度。 ---------------------
+------------- geodotchart base on 以 X 为纬度，Y 为经度。 ---------------------
 
 [ { 
 mag:number, 
 place:string,
 lat:number, 
 lng:number,
-alert:string,  ----geometry.coordinates[3]
-url:string   ---- 如果需要点击跳转的话
+alert:string
+
 },{}....... ]
 
 
 
-------------- data for pie chart ---------------------
+------------- top10countryChart ---------------------
 
-[{alert: string/null, counts:number},{}...]
+[
+{country: string, counts:number}
+,{}...]
 
 
-------------- data for line/bar chart ---------------------
+------------- yearlyBarChart ---------------------
 --by month
 [
 {"month":"Jan","counts":300},{}，{}....
 ]
 
---by country
+------------- seasonalBarChart in 10 days range---------------------
 [
-{"country":"United States","counts":300},{}，{}....
+{"dayrange":"1/1 -1/10","counts":300},
+{"dayrange":"1/11 -1/20","counts":300}，{}....
 ]
 
 */
@@ -48,28 +51,97 @@ url:string   ---- 如果需要点击跳转的话
 var GraphicChart=funciton(){
 	this.radio=1.6;// chart width/height
 	this.charts=[];
-	
+	return this;
+}
+
+/**
+ * Create dot base on depth and timeline
+ */
+
+GraphicChart.prototype.createDepthDotChart=function(data,parentSelector,margin,heading){
+	var depthchart=new DotDiagram().data(data).container(parentSelector);
+	this.charts.push(depthchart);
+	if(margin){
+	    depthchart.margin(margin);
+	}
+	if(heading){
+		depthchart.heading(heading);
+	}
+	depthchart.render(depthchart.depth);
 }
 
 /**
  * Create dot base on latitude and longitude
  */
-
-GraphicChart.prototype.createDepthDotChart=function(data,parentSelector,heading){
-	var geochart=new DotDiagram().data(data).container(parentSelector);
-	this.charts.push(geochart);
-	if(heading){
-		geochart.heading(heading);
-	}
-	geochart.render(geochart.depthAxis);
+GraphicChart.prototype.createGeoDotChart=function(data,parentSelector,margin,heading){
+    var geochart=new DotDiagram().data(data).container(parentSelector);
+    	this.charts.push(geochart);
+    	if(margin){
+    	    geochart.margin(margin);
+    	}
+    	if(heading){
+    		geochart.heading(heading);
+    	}
+    geochart.render(geochart.geo);
 }
 
 /**
- * update its size
+ * Create pie chart base top 10 countries
+ */
+GraphicChart.prototype.createTop10PieChart=function(data,parentSelector,margin,heading){
+    var top10chart=new PieChart().data(data).container(parentSelector);
+    	this.charts.push(top10chart);
+    	if(margin){
+    	    top10chart.margin(margin);
+    	}
+    	if(heading){
+    		top10chart.heading(heading);
+    	}
+    geochart.render(top10chart.country);
+}
+
+
+GraphicChart.prototype.createYearlyBarChart=function(data,parentSelector,margin,heading){
+    var yearlychart= new BarDiagram().data(data).container(parentSelector)；
+    this.charts.push(yearlychart);
+    	if(margin){
+    	    yearlychart.margin(margin);
+    	}
+    	if(heading){
+    		yearlychart.heading(heading);
+    	}
+    yearlychart.render(yearlychart.yearly);
+}
+
+GraphicChart.prototype.createSeasonalBarChart=function(data,parentSelector,margin,heading){
+    var seasonalchart= new BarDiagram().data(data).container(parentSelector)；
+    this.charts.push(seasonalchart);
+    	if(margin){
+    	    seasonalchart.margin(margin);
+    	}
+    	if(heading){
+    		seasonalchart.heading(heading);
+    	}
+    seasonalchart.render(seasonalchart.seasonal);
+}
+
+
+/**
+ * update diagram size
  */
 
 GraphicChart.prototype.update=function(){
 	this.charts.forEach(function(chart){
 		chart.update();
+	});
+}
+
+/**
+* remove all diagram
+*/
+
+GraphicChart.prototype.removeAll=function(){
+	this.charts.forEach(function(chart){
+		chart.remove();
 	});
 }
