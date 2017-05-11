5fa21539a51e91ae476b150b0934d4be7cd5e52c
anna199 <liranlr1992@gmail.com>
2017-05-11 11:07:11 -0700

    Add Chinese translation to famous earthquakes and aboutUs

--- js/text.js
+++ js/text.js
@@ -73,28 +73,28 @@ var historyEarthquakeText = {
     },
     "chinese": {
         "0": {
-            "title": "地震1",
-            "content": "加点内容"
+            "title": "1. 旧金山，加利福尼亚：1906年4月18日（震级：约8级）",
+            "content": "为了应对太平洋和北美构造板块不断增加的压力，在当地时间凌晨5点12分，圣安地列斯断层突然发生了水平滑移，滑移距离超过248英里（400公里）。此次滑移经过城市，震源位于城市外，深度达到6英里（10公里）。 这是地球表面容易观测到的断层滑坡的之中非常罕见的地震之一。 此次地震造成城市大量建筑物倒塌。 尽管大部分木制框架的建筑物幸存于地震，但是这些建筑物仍然毁于城市街区的火灾，火灾造成大量人员伤亡，死亡人数 - 约500人。"
         },
         "1": {
-            "title": "地震2",
-            "content": "加点内容"
+            "title": "2. 东京，日本：1923年9月1日（震级：约8.25）",
+            "content": "在旧金山大地震发生的几年后，一位年轻的日本地震学家试图说服他的老同事，他认为东京应该为像1703年那样非常具有破坏性的地震作准备。 然而他的警告没有被重视。在1923年，由于直接的震荡和火灾，横滨和东京发生了巨大的地震。 约十四万人死亡。"
         },
         "2": {
-            "title": "地震3",
-            "content": "加点内容"
+            "title": "3. 智利：1960年5月21日（震级：约9）",
+            "content": "力将太平洋和南美构造板块推向一起造成的断层面大概有66英尺的断层滑坡，这个断层如此大 - 超过620英里长，大约124英里宽 - 我们把这次地震与整个国家联系起来。在一个世纪的地震时期，这是两次最大的地震之一。当时，几乎没有可以测量非常长的地面波的仪器。但是从现有的记录来看，这次地震后的几天是显而易见的，震动使得整个地球环绕着巨大的钟声。"
         },
         "3": {
-            "title": "地震4",
-            "content": "加点内容"
+            "title": "4. 安克雷奇，阿拉斯加：1964年3月27日（震级：约8.5）",
+            "content": "将太平洋和北美构造板块推向彼此的生长力量使得太平洋板块在阿拉斯加下方的角度下滑了约42英尺，并在安克雷奇港南部的海底下方被摧毁。检测世界各地表面波的地震仪已经敲了几个小时。记录恢复后，他们正在测量地球上持续数天的表面波，仿佛整个地球正在振铃，并且与几乎4年前的智利大地震的频率完全相同。大量的水几乎立即向上或向下移动几码，因为主断层面在海底以下。这次海底运动造成了海啸或水浪，遍及太平洋，造成进一步的损失，甚至在加利福尼亚州的海滩上造成人身伤害。"
         },
         "4": {
-            "title": "地震5",
-            "content": "加点内容"
+            "title": "5. 唐山，中国：1976年7月28日（震级：约7.5）",
+            "content": "中国东部的地震，距北京约100公里，是个煤矿区域，昼夜工作数千人。在唐山人口稠密的城镇，这些建筑物是由重型砖石构成的，没有采取特别的预防措施来抵御地震。这场地震约有24万人死亡。在世界这个地区，构造板块之间的界限没有明确定义。相反，它看起来像一个数百万平方英里的范围在变形或改变区域的形状，这不是一个稳定的过程，是由于地震。"
         },
         "5": {
-            "title": "地震6",
-            "content": "加点内容"
+            "title": "6. 苏门答腊，印度尼西亚，2004年12月26日（震级：约9.0）",
+            "content": "一场9.0级的地震是40年来最大的地震，造成了印度洋的强大的海啸，破坏了12个亚洲国家。地震的震中位于印度尼西亚苏门答腊岛的西海岸。一个星期后，死亡人数估计为15万人 - 一个月后，有一次估计是这个数字的两倍。印度尼西亚（特别是亚齐省），斯里兰卡，印度，泰国和马尔代夫受到的打击最大。数百万人失去家园。由于整个家庭和城镇都被摧毁，尸体被扫射到海上，所以有可能我们永远都不会知道失去的总人数。"
         }
     }
 };
@@ -102,10 +102,10 @@ var historyEarthquakeText = {
 var aboutUsText = {
     "english": {
         "title": "This project is designed to raise attention of the audience on earthquake hazards.",
-        "content": "In order to raise awareness of the public, our project has created a 3D earth and historical earthquake data points are added to 3D earth accordingly. Graphical analysis of earthquake magnitude v.s. time, number of earthquakes v.s. monthly distribution, earthquake magnitude v.s. Geolocation are provided to user for additional information . Users can also filter earthquake magnitude and countries information accordingly. Informations such as 6 famous earthquakes has also been added to the website feature. "
+        "content": "In order to raise awareness of the public, our project has created a 3D earth and historical earthquake data points are added to 3D earth accordingly. Graphical analysis of earthquake magnitude v.s. time, number of earthquakes v.s. monthly distribution, earthquake magnitude v.s. Geolocation are provided to user for additional information . Users can also filter earthquake countries information accordingly. Informations such as 6 famous earthquakes has also been added to the website feature. "
     },
     "chinese": {
-        "title": "关于我们标题",
-        "content": "关于我们内容"
+        "title": "关于我们",
+        "content": "为了提高公众的意识，我们的项目已经创建了一个3D地球，并且相应地将3D地球的地震数据点添加到了3D地球。地震震级的图形分析时间，地震数量月度分布，地震震级向用户提供地理位置以获取更多信息。用户还可以相应地过滤地震国家信息。网站功能也添加了6次着名地震等信息。"
     }
 };
